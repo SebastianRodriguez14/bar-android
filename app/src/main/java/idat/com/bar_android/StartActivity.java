4dd11439963ecc4eecc8982f141e05e12a7eb189
Saiyan2003 <samuelcraft2003@gmail.com>
2022-08-04 23:58:09 -0500

    diseño parte 1

---
+++ app/src/main/java/idat/com/bar_android/StartActivity.java
@@ -0,0 +1,32 @@
+package idat.com.bar_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
+
+public class StartActivity extends AppCompatActivity {
+
+    MaterialButton materialButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        materialButton = findViewById(R.id.button_vamos);
+
+        materialButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StartActivity.this , LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+}
