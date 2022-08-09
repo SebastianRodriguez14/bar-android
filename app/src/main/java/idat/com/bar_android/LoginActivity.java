f245a77f6d4279bb3e0049560f9f349d7a083dab
tatiana0326 <tatiana05taboada@gmail.com>
2022-08-09 00:53:00 -0500

    Menu Opciones Pedido

--- app/src/main/java/idat/com/bar_android/LoginActivity.java
+++ app/src/main/java/idat/com/bar_android/LoginActivity.java
@@ -3,6 +3,7 @@ package idat.com.bar_android;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class LoginActivity extends AppCompatActivity {
 
     TextView textView;
+    Button btnMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +19,21 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         textView = findViewById(R.id.text_register);
+        btnMenu = findViewById(R.id.btnAccederMenu);
 
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(LoginActivity.this , RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btnMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this , MenuActivity.class);
                 startActivity(intent);
                 finish();
             }
