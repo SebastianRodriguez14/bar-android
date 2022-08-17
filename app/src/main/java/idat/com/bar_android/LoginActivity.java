384318dd70cd101864dce99aa55395699ccefca9
Quispe-E <quispeaguila07@gmail.com>
2022-08-17 09:52:30 -0500

    Inicio o registro con sqlite

--- app/src/main/java/idat/com/bar_android/LoginActivity.java
+++ app/src/main/java/idat/com/bar_android/LoginActivity.java
@@ -2,13 +2,20 @@ package idat.com.bar_android;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import idat.com.bar_android.db.DbUsers;
+
 public class LoginActivity extends AppCompatActivity {
+    //agregado--------------------
+    EditText inputEmail, inputPassword;
 
     TextView textView;
     Button btnMenu;
@@ -17,6 +24,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //agregado----------
+        inputEmail = findViewById(R.id.editText_Correo);
+        inputPassword = findViewById(R.id.editText_Clave);
 
         textView = findViewById(R.id.text_register);
         btnMenu = findViewById(R.id.btnAccederMenu);
@@ -33,11 +44,40 @@ public class LoginActivity extends AppCompatActivity {
         btnMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this , MenuActivity.class);
-                startActivity(intent);
-                finish();
+                String email = inputEmail.getText().toString();
+                String password = inputPassword.getText().toString();
+                System.out.println(" DATOS DEL USUARIO");
+                System.out.println("Correo: " + email);
+                System.out.println("Contraseña: " + password);
+                if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+                    Toast.makeText(LoginActivity.this, "Complete los campos", Toast.LENGTH_SHORT).show();
+                }else {
+                    DbUsers dbUsers = new DbUsers(LoginActivity.this);
+                    boolean exists = dbUsers.isUserExists(email, password);
+                    if (exists) {
+                        Toast.makeText(LoginActivity.this, "Usuario ya existente", Toast.LENGTH_SHORT).show();
+                    } else {
+                        long id = dbUsers.insertUser(email, password);
+                        if (id > 0) {
+                            Toast.makeText(LoginActivity.this, "Usuario creado", Toast.LENGTH_SHORT).show();
+                            limpiarCampos();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Usuario no creado", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    Intent intent = new Intent(LoginActivity.this, MenuActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
             }
         });
 
+    }
+
+
+    private void limpiarCampos(){
+        inputEmail.setText("");
+        inputPassword.setText("");
     }
 }
